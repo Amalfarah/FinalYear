795771f4cc2082855d466e06337d09ddde701060
Amalfarah <1712612@brunel.ac.uk>
2020-02-09 11:34:02 +0000

    uses user location

--- Python/ln.py
+++ Python/ln.py
@@ -4,22 +4,36 @@ from datetime import datetime
 import time
 from datetime import timedelta
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/location")
-def l():
-    
 #uses user location and the chosen destination
 #takes in the users location using IP address and the destination
-    g = geocoder.ip('me')
-    p = geocoder.location(input())
+g = geocoder.ip('me')
+p = geocoder.location(input())
 #finds the mile between the two locations and the hour it takes
-    j = (geodesic(g.latlng,p.latlng).mi)*(geodesic(1).mi)
+j = (geodesic(g.latlng,p.latlng).mi)*(geodesic(1).mi)
 #finds the current time (including timezone and date)
-    now = datetime.now()
-    delta = timedelta(hours=j)
+now = datetime.now()
+delta = timedelta(hours=j)
 #adds the current time to j 
-    u=now+delta
+u=now+delta
+
+print("Miles between destinations is:",geodesic(g.latlng,p.latlng).mi,"miles")
+print("Journey will roughly  take:",round(j))
+print("departure Time:",now,"Arrival Time:",u)
+print()
+print()
+
+#uses user location and the chosen destination
+#takes in the users location using IP address and the destination
+g = geocoder.location(input())
+p = geocoder.location(input())
+#finds the mile between the two locations and the hour it takes
+j = (geodesic(g.latlng,p.latlng).mi)*(geodesic(1).mi)
+#finds the current time (including timezone and date)
+now = datetime.now()
+delta = timedelta(hours=j)
+#adds the current time to j 
+u=now+delta
 
 print("Miles between destinations is:",geodesic(g.latlng,p.latlng).mi,"miles")
 print("Journey will roughly  take:",round(j))
