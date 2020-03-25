07d78aa361c62996cb56501051744bebf767bf39
Amalfarah <1712612@brunel.ac.uk>
2020-03-25 18:03:05 +0000

    old version

---
+++ Unwanted code/GBVReportApp/app/src/main/java/com/example/cs17aaf1/gbvreportapp/optionspage.java
@@ -0,0 +1,13 @@
+package com.example.cs17aaf1.gbvreportapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class optionspage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_optionspage);
+    }
+}
